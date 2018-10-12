fcc0d96b2d1ddf3f90ebb978a4349ee2fb60ecb8
David Soto <augusto.soto@udea.edu.co>
2018-10-12 04:12:07 -0500

    refactor

--- camara.c
+++ camara.c
@@ -23,6 +23,13 @@ using namespace std;
 #define ROTATE_RIGTH 'e'
 #define ROTATE_LEFT 'q'
 
+class relativeSpace {
+	public:
+		// static Vector3d less(Point3d a, Point3d b) {
+		// 	int x = abs(a.get_x) 
+		// }
+};
+
 class Point3d {
 	private:
 		GLfloat x, y, z;
@@ -50,16 +57,96 @@ class Point3d {
 		}
 };
 
+class Vector3d {
+	private:
+		GLfloat x;
+		GLfloat y;
+		GLfloat z;
+
+		GLfloat xyAngle;
+		GLfloat xzAngle;
+		GLfloat yzAngle;
+	public:
+
+		Vector3d(GLfloat x = 0.0, GLfloat y = 0.0, GLfloat z = 0.0, GLfloat xyAngle = 0.0, GLfloat xzAngle = 0.0, GLfloat yzAngle = 0.0){
+			this->x = x;
+			this->y = y;
+			this->z = z;
+			this->xyAngle = caculateDegrees(xyAngle);
+			this->xzAngle = caculateDegrees(xzAngle);
+			this->yzAngle = caculateDegrees(yzAngle);
+		}
+
+		GLfloat get_x  		(void) 	{return x;}
+		GLfloat get_y		(void) 	{return y;}
+		GLfloat get_z		(void) 	{return z;}
+		GLfloat get_xyAngle	(void) 	{return xyAngle;}
+		GLfloat get_xzAngle	(void) 	{return xzAngle;}
+		GLfloat get_yzAngle	(void) 	{return yzAngle;}
+
+		void set_x		 (GLfloat x) 		{this->x = x;}
+		void set_y		 (GLfloat y) 		{this->y = y;}
+		void set_z		 (GLfloat z) 		{this->z = z;}
+		void set_xyAngle (GLfloat xyAngle) 	{this->xyAngle = xyAngle;}
+		void set_xzAngle (GLfloat xzAngle) 	{this->xzAngle = xzAngle;}
+		void set_yzAngle (GLfloat yzAngle) 	{this->yzAngle = yzAngle;}
+
+
+		GLfloat caculateDegrees(GLfloat dumbDegrees) {	
+			GLfloat degrees = (dumbDegrees < 0 ? 360: 0) + dumbDegrees;
+			degrees =  (int)degrees % 360;
+			return (GLfloat)degrees;
+		}
+
+		GLfloat toRadians(GLfloat degrees) {
+			return ( degrees * (PI / 180.0) );
+		}
+
+		void add_xzAngle(int a) {
+			xzAngle = caculateDegrees(xzAngle + a);
+			GLfloat hip = sqrt(pow(x,2) + pow(z,2));
+			x = cos(toRadians(xzAngle));
+			z = sin(toRadians(xzAngle));
+		}
+		void add_xyAngle(int a) {
+			xyAngle = caculateDegrees(xyAngle + a);
+			GLfloat hip = sqrt(pow(x,2) + pow(y,2));
+			x = cos(toRadians(xyAngle));
+			y = sin(toRadians(xyAngle));
+		}
+
+		// void add_yzAngle(int a) {
+		// 	yzAngle = caculateDegrees(yzAngle + a);
+		// 	y = yComponent();
+		// 	z = zComponent();
+		// }
+
+		void print() {
+			cout << "Im an vector\n";
+			cout << "--------------------------------\n";
+			printf("components: (%f, %f, %f)\n", x, y, z);
+
+			printf("xyAngle: %f\n", xyAngle);
+			printf("xzAngle: %f\n", xzAngle);
+			printf("yzAngle: %f\n", yzAngle);
+
+			cout << "--------------------------------\n\n";
+		}
+};
+
 class Entity {
 	protected:
 		Point3d center;
 		GLboolean isSolid;
+		GLboolean isLive;
+		Vector3d target;
 		int id;
 	public:
-		Entity (int idEnt, Point3d p, GLboolean s = true) {
+		Entity (Point3d p, GLboolean s = false, GLboolean live = false, int idEnt = 0) {
 			id = idEnt;
 			center = p;
 			isSolid = s;
+			isLive = live;
 		}
 
 		Point3d 	get_point(void) 			{return center;}
@@ -67,6 +154,21 @@ class Entity {
 		void 		set_point(Point3d p) 		{center = p;}
 		void 		set_isSolid(GLboolean s)	{isSolid = s;}
 
+		void addTarget(Point3d target) {
+			if (isLive) {
+				// this->target = target;
+			}
+		}
+
+		virtual Entity& live() {
+			isLive = true;
+			return *this;
+		}
+
+		virtual Entity& solid() {
+			isSolid = true;
+			return *this;
+		}
 
 		virtual void print() {
 			cout << "Im an entity\n";
@@ -84,8 +186,8 @@ class Box: public Entity {
 	private:
 		GLfloat heigth, width, deep;
 	public:
-		Box(int id, Point3d p, GLfloat w, GLfloat h, GLfloat d)
-		: Entity(id, p){
+		Box(Point3d p, GLfloat w, GLfloat h, GLfloat d, GLboolean s = false, GLboolean live = false,int id = 0)
+		: Entity(p, s, live, id){
 			heigth = h;
 			width = w;
 			deep = d;
@@ -106,6 +208,16 @@ class Box: public Entity {
 			glPopMatrix( );
 		}
 
+		Box& live() {
+			isLive = true;
+			return *this;
+		}
+
+		Box& solid() {
+			isSolid = true;
+			return *this;
+		}
+
 		void print() {
 			cout << "Im a box\n";
 			cout << "--------------------------------\n";
@@ -123,19 +235,15 @@ class Box: public Entity {
 
 
 class EntitiesFactory {
-	private:
-		static int idEntity;
 	public:
 		static Entity entity(Point3d center) {
-			idEntity += 1;
-			return Entity(idEntity, center);
+			return Entity(center);
 		}
+
 		static Box box(Point3d center, GLfloat heigth, GLfloat width, GLfloat deep) {
-			idEntity += 1;
-			return Box(idEntity, center, heigth, width, deep);
+			return Box(center, heigth, width, deep);
 		}
 };
-int EntitiesFactory::idEntity = 0;
 
 //Singleton
 class World {
@@ -181,7 +289,7 @@ World* world = World::getInstance();
 GLenum doubleBuffer;
 GLfloat worldVelocity = 0;
 
-Entity *entity;
+Vector3d vision(0.0, 0.0, -1.0, 0.0, 270.0);
 
 GLfloat zCam = 10.0, 
 		xCam = 0.0,
@@ -206,15 +314,8 @@ GLfloat toRadians(GLfloat degrees) {
 }
 
 void caculateCamRotation(void) {
-	GLfloat hip = sqrt( pow(zCam, 2) + pow(xCam, 2) );
-	GLfloat rads = toRadians(xzAngle);	
-	xEye = xCam + hip*cos(rads);
-	zEye = zCam + hip*sin(rads);
-}
-
-void updateVelocityGravity() {
-	yCam += yVelocity;
-	yVelocity += GRAVITY;
+	xEye = xCam + vision.get_x();
+	zEye = zCam + vision.get_z();
 }
 
 void init(void) {
@@ -229,9 +330,9 @@ void display(void) {
    	glColor3f (1.0, 0.0, 0.0);
    	glLoadIdentity ();     
 		   
-   	gluLookAt (xCam, yCam, zCam,  // cam position
-   			  xEye , yEye, zEye, // target's cam 
-			  0.0, 1.0, 0.0);   // up
+   	gluLookAt (xCam, yCam, zCam,  								// cam position
+   			  xEye , yEye, zEye, 	// target's cam 
+			  0.0, 1.0, 0.0);   								// up
 
 	world->makeWorld();
 	
@@ -241,35 +342,20 @@ void display(void) {
 
 static void Key(unsigned char key, int x, int y) {
 
-	GLfloat rads = toRadians(xzAngle);	
-
-	//Vector unitario para movimiento hacia adelante y hacia atras.
-	GLfloat X_FBmove = cos(rads);
-	GLfloat Z_FBmove = sin(rads);
-
 	//Vector unitario para movimientos laterales
 	GLfloat toRad90 = toRadians(90.0);
-	GLfloat X_Lmove = X_FBmove*cos(toRad90) - Z_FBmove*sin(toRad90);
-	GLfloat Z_Lmove = X_FBmove*sin(toRad90) + Z_FBmove*cos(toRad90);
+	GLfloat X_Lmove = vision.get_x()*cos(toRad90) - vision.get_z()*sin(toRad90);
+	GLfloat Z_Lmove = vision.get_x()*sin(toRad90) + vision.get_z()*cos(toRad90);
 
 	switch (key) {
-
-		//rotational moves
-		case ROTATE_RIGTH:
-			xzAngle = caculateDegrees( xzAngle + ROTATIONAL_SPEED);
-			break;
-		case ROTATE_LEFT:
-			xzAngle = caculateDegrees( xzAngle - ROTATIONAL_SPEED);
-			break;
-
 		// lineal moves
 		case FRONT_MOVE:
-			zCam += Z_FBmove*LINEAL_SPEED;
-			xCam += X_FBmove*LINEAL_SPEED;
+			zCam += vision.get_z()*LINEAL_SPEED;
+			xCam += vision.get_x()*LINEAL_SPEED;
 			break;
 		case BACK_MOVE:
-			zCam -= Z_FBmove*LINEAL_SPEED;
-			xCam -= X_FBmove*LINEAL_SPEED;
+			zCam -= vision.get_z()*LINEAL_SPEED;
+			xCam -= vision.get_x()*LINEAL_SPEED;
 			break;
 		case RIGTH_MOVE:
 			zCam += Z_Lmove*LINEAL_SPEED;
@@ -284,6 +370,21 @@ static void Key(unsigned char key, int x, int y) {
 		case 27:
 			exit(0);
     }
+}
+
+void SpecialInput(int key, int x, int y){
+switch(key)
+{
+
+case GLUT_KEY_LEFT:
+vision.add_xzAngle( -ROTATIONAL_SPEED );
+break;
+case GLUT_KEY_RIGHT:
+vision.add_xzAngle(  ROTATIONAL_SPEED );
+break;
+}
+
+glutPostRedisplay();
 }
 
 void reshape (int w, int h) {
@@ -305,30 +406,33 @@ static void Args(int argc, char **argv) {
       doubleBuffer = GL_TRUE;
     }
   }
-
 }
 
 void createInitWorld() {
-	Box box2  = EntitiesFactory::box(Point3d(-10.0, 0.0, 0.0), 7.0, 17.0, 7.0);
-	Box box3 = EntitiesFactory::box(Point3d(10.0, 0.0, 0.0), 7.0, 7.0, 7.0);
-	Entity entity = EntitiesFactory::entity(Point3d(0.0, 0.0, 0.0));
 
+	Box box1  		= EntitiesFactory::box(Point3d(0.0, 0.0, -14.0), 4.0, 4.0, 4.0).solid();
+	Box box2  		= EntitiesFactory::box(Point3d(-10.0, 0.0, 0.0), 7.0, 17.0, 7.0).solid();
+	Box box3 		= EntitiesFactory::box(Point3d(10.0, 0.0, 0.0), 7.0, 7.0, 7.0).solid();
+
+	Entity player 	= EntitiesFactory::entity(Point3d(0.0, 0.0, 10.0)).solid().live();
+	player.addTarget(Point3d(0.0, 0.0, 0.0));
+
+   	world->addEntity(&box1);
    	world->addEntity(&box2);
    	world->addEntity(&box3);
-	world->addEntity(&entity);
+	world->addEntity(&player);
 
 	init ();
 	glutKeyboardFunc(Key);
+	glutSpecialFunc(SpecialInput);
 	glutDisplayFunc(display); 
 	glutReshapeFunc(reshape);
 	glutIdleFunc(display);  
 	glutMainLoop();
+
 }
 
 int main(int argc, char** argv) {
-
-	Box box = EntitiesFactory::box(Point3d(10.0, 0.0, 0.0), 7.0, 15.0, 7.0);
-	entity = &box;
 
 	GLenum type;
 
